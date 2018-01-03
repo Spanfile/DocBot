bb9d8fb44aa2554269672013b4a22600a918970a
Spanfile <santeri.kasvi@gmail.com>
2018-01-03 16:46:14 +0200

    Improve help module

--- DocBot/Modules/HelpModule.cs
+++ DocBot/Modules/HelpModule.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace DocBot.Modules
 {
+    [Name("Help")]
     public class HelpModule : ModuleBase<SocketCommandContext>
     {
         private readonly CommandService service;
@@ -19,6 +20,7 @@ namespace DocBot.Modules
         }
 
         [Command("help")]
+        [Summary("Shows this help")]
         public async Task HelpAsync()
         {
             var prefix = config["prefix"];
@@ -48,7 +50,42 @@ namespace DocBot.Modules
                 }
             }
 
-            await ReplyAsync("", false, builder.Build());
+            await ReplyAsync("", embed: builder.Build());
+        }
+
+        [Command("help")]
+        [Summary("Shows help for a certain command")]
+        public async Task HelpAsync(string commandStr)
+        {
+            var result = service.Search(Context, commandStr);
+
+            if (!result.IsSuccess)
+            {
+                await ReplyAsync($"What is `{commandStr}`?");
+                return;
+            }
+
+            var prefix = config["prefix"];
+            var builder = new EmbedBuilder {
+                Color = new Color(100, 149, 237),
+                Description = $"These commands look like `{commandStr}`"
+            };
+
+            foreach (var match in result.Commands)
+            {
+                var command = match.Command;
+
+                builder.AddField(f =>
+                {
+                    f.Name = string.Join(", ", command.Aliases);
+                    f.Value =
+                        $"Parameters: {string.Join(", ", command.Parameters.Select(p => p.Name))}" +
+                        $"Summary: {command.Summary}";
+                    f.IsInline = false;
+                });
+            }
+
+            await ReplyAsync("", embed: builder.Build());
         }
     }
 }
