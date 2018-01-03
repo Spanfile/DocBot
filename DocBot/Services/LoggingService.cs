fa335c063c22378c9700867b6964bca27be84f6b
Spanfile <santeri.kasvi@gmail.com>
2018-01-03 16:45:44 +0200

    Don't write to a separate log file

--- DocBot/Services/LoggingService.cs
+++ DocBot/Services/LoggingService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -12,13 +11,8 @@ namespace DocBot.Services
         private readonly DiscordSocketClient discord;
         private readonly CommandService commands;
 
-        private readonly string logDirectory;
-        private string LogFile => Path.Combine(logDirectory, $"{DateTime.UtcNow:yyyy-MM-dd}.txt");
-
         public LoggingService(DiscordSocketClient discord, CommandService commands)
         {
-            logDirectory = Path.Combine(AppContext.BaseDirectory, "logs");
-
             this.discord = discord;
             this.commands = commands;
 
@@ -33,21 +27,9 @@ namespace DocBot.Services
         public Task LogVerbose(string message, string source = "DocBot", Exception ex = null) => Log(LogSeverity.Verbose, source, message, ex);
         public Task LogDebug(string message, string source = "DocBot", Exception ex = null) => Log(LogSeverity.Debug, source, message, ex);
 
-        private Task Log(LogSeverity severity, string source, string message, Exception ex) =>
-            OnLog(new LogMessage(severity, source, message, ex));
+        private static Task Log(LogSeverity severity, string source, string message, Exception ex) => OnLog(new LogMessage(severity, source, message, ex));
 
-        private Task OnLog(LogMessage message)
-        {
-            if (!Directory.Exists(logDirectory))
-                Directory.CreateDirectory(logDirectory);
-
-            if (!File.Exists(LogFile))
-                File.Create(LogFile).Dispose();
-
-            var logText =
-                $"{DateTime.UtcNow:hh:mm:ss} [{message.Severity}] {message.Source}: {message.Exception?.ToString() ?? message.Message}";
-
-            return Task.WhenAll(File.AppendAllTextAsync(LogFile, logText + '\n'), Console.Out.WriteLineAsync(logText));
-        }
+        private static Task OnLog(LogMessage message) =>
+            Console.Out.WriteLineAsync($"{DateTime.UtcNow:hh:mm:ss} [{message.Severity}] {message.Source}: {message.Exception?.ToString() ?? message.Message}");
     }
 }
