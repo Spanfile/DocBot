ba49e361880480b3c194542759a961a181e1ae84
Spanfile <santeri.kasvi@gmail.com>
2018-01-22 18:33:27 +0200

    Begin JsonIndexDocumentationProvider

---
+++ DocBot/Services/Documentation/JsonIndexDocumentationProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocBot.Services.Documentation
+{
+    internal abstract class JsonIndexDocumentationProvider : DocumentationProvider
+    {
+        public abstract string IndexLocation { get; }
+
+        protected bool IndexInitialised = false;
+
+        protected JsonIndexDocumentationProvider(IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+        }
+
+        private void InitialiseIndex()
+        {
+            if (IndexInitialised)
+                return;
+
+
+        }
+
+        public override Task<IReadOnlyList<DocumentationArticle>> SearchArticles(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
