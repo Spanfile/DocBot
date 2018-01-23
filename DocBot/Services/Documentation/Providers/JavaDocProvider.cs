c6a9366ad8bcdc849e8fb513639292f54824a415
Spanfile <santeri.kasvi@gmail.com>
2018-01-23 19:56:04 +0200

    Naive search for Java doc index

--- DocBot/Services/Documentation/Providers/JavaDocProvider.cs
+++ DocBot/Services/Documentation/Providers/JavaDocProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -9,9 +10,13 @@ namespace DocBot.Services.Documentation.Providers
     {
         private struct IndexObject
         {
+            [JsonProperty(PropertyName = "full_name")]
             public string FullName;
+            [JsonProperty(PropertyName = "url")]
             public string Url;
+            [JsonProperty(PropertyName = "object_type")]
             public string ObjectType;
+            [JsonProperty(PropertyName = "description")]
             public string Description;
 
             public IndexObject(string fullName, string url, string objectType, string description)
@@ -37,12 +42,67 @@ namespace DocBot.Services.Documentation.Providers
         {
             var serialiser = new JsonSerializer();
             var index = serialiser.Deserialize<IndexObject[]>(reader);
-
             await Logger.LogDebug($"{index.Length} objects loaded from index", "JavaDocProvider");
 
+            var queryArgs = query.ToLowerInvariant().Split('.');
+            var actualQuery = queryArgs[queryArgs.Length - 1];
+
+            var matches = new List<(int, IndexObject)>();
             foreach (var indexObj in index)
             {
-                
+                var nameArgs = indexObj.FullName.ToLowerInvariant().Split('.');
+                var exactIndex = Array.IndexOf(nameArgs, actualQuery);
+
+                if (exactIndex == -1) // exact match not found, check for the last elements
+                {
+                    var lastName = nameArgs[nameArgs.Length - 1];
+                    if (lastName.Contains(actualQuery))
+                    {
+                        // query found as substring - rank based on length difference: larger length difference = less equal match
+                        matches.Add((lastName.Length - actualQuery.Length, indexObj));
+                    }
+                }
+                else // exact index found, rank based on how close to the end the match is by characters
+                {
+                    matches.Add((nameArgs.Skip(exactIndex + 1).Sum(s => s.Length), indexObj));
+                }
+            }
+
+            await Logger.LogDebug($"{matches.Count} matches found", "JavaDocProvider");
+
+            var sorted =
+                from match in matches
+                orderby match.Item1, ObjectTypeToInt(match.Item2.ObjectType), match.Item2.FullName
+                select new DocumentationArticle(match.Item2.FullName, match.Item2.Url, match.Item2.Description,
+                    match.Item2.ObjectType);
+
+            return sorted;
+        }
+
+        private int ObjectTypeToInt(string objectType)
+        {
+            switch (objectType)
+            {
+                case "Class":
+                    return 0;
+
+                case "Interface":
+                    return 1;
+
+                case "Constructor":
+                    return 2;
+
+                case "Method":
+                    return 3;
+
+                case "Constant":
+                    return 4;
+
+                case "Variable":
+                    return 5;
+
+                default:
+                    return int.MaxValue;
             }
         }
     }
