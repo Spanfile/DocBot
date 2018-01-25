c06c94e2e0b29fdce8c3971d6746a4ec06cb3d48
Spanfile <santeri.kasvi@gmail.com>
2018-01-25 19:39:51 +0200

    Begin ArchWiki provider

---
+++ DocBot/Services/Documentation/Providers/ArchWikiProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DocBot.Services.Documentation.Providers
+{
+    internal class ArchWikiProvider : JsonDocumentationProvider
+    {
+        public override string FriendlyName => "ArchWiki";
+        public override string[] Aliases => new[] {"arch", "archwiki", "linux"};
+
+        public override string SearchURLFormat =>
+            "https://wiki.archlinux.org/api.php?action=opensearch&format=json&formatversion=2&search={0}&namespace=0&limit=10";
+
+        public override string BaseURL => "https://wiki.archlinux.org/";
+
+        public ArchWikiProvider(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        protected override IEnumerable<DocumentationArticle> InternalGetDocumentationArticles(JsonTextReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
